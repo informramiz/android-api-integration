66b4d80d65142d908bced01bc1a370b35b3f4755
Ramiz Raja <informramiz@gmail.com>
2019-07-11 21:47:51 +0500

    feat: Add dagger AppComponent

---
+++ app/src/main/java/github/informramiz/com/androidapiintegration/di/components/AppComponent.kt
@@ -0,0 +1,32 @@
+package github.informramiz.com.androidapiintegration.di.components
+
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.support.AndroidSupportInjectionModule
+import github.informramiz.com.androidapiintegration.AppApplication
+import javax.inject.Singleton
+
+
+/**
+ * Created by Ramiz Raja on 2019-07-11.
+ */
+@Singleton
+@Component(
+    modules = [AndroidInjectionModule::class,
+        AndroidSupportInjectionModule::class]
+)
+interface AppComponent {
+    //we are defining custom build to provide it with AppApplication using
+    //method `fun application(application: AppApplication): Builder`
+    //so that it later can be used for injection in other classes
+    @Component.Builder
+    interface Builder {
+        //bind `AppApplication` instance to injector
+        @BindsInstance
+        fun application(application: AppApplication): Builder
+        fun build(): AppComponent
+    }
+
+    fun inject(application: AppApplication)
+}
