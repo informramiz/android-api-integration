ddb76b0bf1387da2aff4e3c01536e83b21f57507
Ramiz Raja <informramiz@gmail.com>
2019-07-11 22:05:14 +0500

    feat: Add BaseViewModel

---
+++ app/src/main/java/github/informramiz/com/androidapiintegration/ui/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package github.informramiz.com.androidapiintegration.ui.base
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * Created by Ramiz Raja on 2019-07-11.
+ */
+class BaseViewModel : ViewModel()
