e510b0c0f0a47825b2c2c9eca16785533f96a6b8
Ramiz Raja <informramiz@gmail.com>
2019-07-11 22:07:14 +0500

    feat: Make MainViewModel injectable

--- app/src/main/java/github/informramiz/com/androidapiintegration/ui/main/MainViewModel.kt
+++ app/src/main/java/github/informramiz/com/androidapiintegration/ui/main/MainViewModel.kt
@@ -1,7 +1,8 @@
 package github.informramiz.com.androidapiintegration.ui.main
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
+
 }
