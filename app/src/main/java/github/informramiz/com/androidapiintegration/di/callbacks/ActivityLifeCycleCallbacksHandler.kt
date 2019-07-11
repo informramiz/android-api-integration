5275606eff664a40ffa241525abc8502ece3118c
Ramiz Raja <informramiz@gmail.com>
2019-07-11 21:45:00 +0500

    feat: Add wrapper for app lifecycle callbacks

---
+++ app/src/main/java/github/informramiz/com/androidapiintegration/di/callbacks/ActivityLifeCycleCallbacksHandler.kt
@@ -0,0 +1,38 @@
+package github.informramiz.com.androidapiintegration.di.callbacks
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+/**
+ * Created by Ramiz Raja on 2019-06-18
+ */
+internal open class ActivityLifeCycleCallbacksHandler : Application.ActivityLifecycleCallbacks {
+    override fun onActivityPaused(activity: Activity?) {
+
+    }
+
+    override fun onActivityResumed(activity: Activity?) {
+
+    }
+
+    override fun onActivityStarted(activity: Activity?) {
+
+    }
+
+    override fun onActivityDestroyed(activity: Activity?) {
+
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+
+    }
+
+    override fun onActivityStopped(activity: Activity?) {
+
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+
+    }
+}
