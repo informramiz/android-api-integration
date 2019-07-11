2d25b61d58a78c40e6c2b298f233a37e41352390
Ramiz Raja <informramiz@gmail.com>
2019-07-11 22:36:54 +0500

    refactor: Use ktx to get view model

--- app/src/main/java/github/informramiz/com/androidapiintegration/ui/main/MainFragment.kt
+++ app/src/main/java/github/informramiz/com/androidapiintegration/ui/main/MainFragment.kt
@@ -1,8 +1,6 @@
 package github.informramiz.com.androidapiintegration.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,7 +13,7 @@ class MainFragment : BaseFragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel by appViewModels<MainViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -24,7 +22,6 @@ class MainFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
 }
