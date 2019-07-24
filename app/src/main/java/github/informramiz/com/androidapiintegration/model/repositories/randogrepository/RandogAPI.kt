267303d85a3c563eeadc2a9f638fe0c58cf3bdbc
Ramiz Raja <informramiz@gmail.com>
2019-07-24 20:23:55 +0500

    refactor: Remove unnecessary method

--- app/src/main/java/github/informramiz/com/androidapiintegration/model/repositories/randogrepository/RandogAPI.kt
+++ app/src/main/java/github/informramiz/com/androidapiintegration/model/repositories/randogrepository/RandogAPI.kt
@@ -8,12 +8,6 @@ import retrofit2.http.GET
  * Created by Ramiz Raja on 2019-07-23
  */
 interface RandogAPI {
-    companion object {
-        fun posterUrl(posterPath: String): String {
-            //Format: https://dog.ceo/api/breed/hound/images
-            return "https://dog.ceo/api/breed/$posterPath/images"
-        }
-    }
     @GET("breeds/list/all")
     suspend fun listBreeds(): Response<BreedsListResponse>
 }
